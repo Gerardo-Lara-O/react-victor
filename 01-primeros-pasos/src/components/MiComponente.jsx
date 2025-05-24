959d5866fec16e9d1b9cbcae079be741522f34db
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2025-05-23 21:10:15 -0600

    Como crear un componente

---
+++ 01-primeros-pasos/src/components/MiComponente.jsx
@@ -0,0 +1,18 @@
+// Importar modulos de react / dependencias
+import {React} from "react";
+
+// Funcion del componente
+const MiComponente = () => {
+
+    // Dentro del componente vamos a tener un return que va a devolver el HTML que yo quiero mostrar por pantalla
+    // Para agregra mas etiquetas dentro del return usamos ()
+    return(
+        <>
+            <h2>Componente Creado</h2>
+            <p>Este es mi primer componente</p>
+        </>
+    )
+}
+
+// Exporta el componente
+export default MiComponente
---
+++ 01-primeros-pasos/src/components/Saludo.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+function Saludo(){
+    
+    return(
+        <>
+            <h2>Hola</h2>
+        </>
+    )
+}
+
+export default Saludo
